2a23d7b0133969c58ed8e84d788cf3a2ce4b9a5b
Joaquin Cabezas <joaquin.cabezas.berjillos.al@iespoligonosur.org>
2024-09-26 14:44:35 +0200

    Actualizacion de ejercicios hechos en clase

---
+++ DAW (Mercedes)/Boletin 1/ejercicio10.js
@@ -0,0 +1,6 @@
+/*Realizar un programa que proporcione el cambio de Euros a Dólares, Libras, Yenes, Franco Suizo, Corona Noruega, según opción. 
+El programa debe controlar todas las entradas y ofrecer al usuario la posibilidad de repetir o salir.*/
+
+let num = parseInt(prompt("Introduce la cantidad de Euros que quieres convertir",0));
+
+let moneda = prompt("¿A que otra moneda quieres pasarlo? Escribe Dólares, Libras, Yenes, Franco Sizo o Corona Noruega",0);
