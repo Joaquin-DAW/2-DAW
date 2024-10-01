03b4a1668549459c197d330dd714764ec1a6a06f
Joaquin Cabezas <joaquin.cabezas.berjillos.al@iespoligonosur.org>
2024-10-01 09:02:41 +0200

    Prueba

--- DWES (Jorge)/Tema_2/myvenv/bin/activate.fish
+++ DWES (Jorge)/Tema_2/myvenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alumnado/Documentos/2-DAW/DWES (Jorge)/Tema_2/myvenv"
+set -gx VIRTUAL_ENV "/home/alumnado/Documentos/Carpeta de Git/2-DAW/DWES (Jorge)/Tema_2/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
